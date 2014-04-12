0d0b4cdf5069c7e37cb1d6bfce63283130b62d46
jorge.silva@thejsj.com <jorge.silva@thejsj.com>
2014-04-12 17:45:25 -0400

    Adding Styles

--- onedollarpayments/forms.py
+++ onedollarpayments/forms.py
@@ -10,15 +10,31 @@ class RequestSenderForm(forms.ModelForm):
         model = Sender
         fields = ['email', 'bank_routing_number', 'bank_account_number']
 
+    def __init__(self, *args, **kwargs):
+        super(RequestSenderForm, self).__init__(*args, **kwargs)
+        self.fields['email'].label = "Your Email Address"
+        self.fields['bank_routing_number'].label = "Your Bank Routing Number"
+        self.fields['bank_account_number'].label = "Your Bank Account Number"
+
 class RequestRecepientForm(forms.ModelForm):
     class Meta:
         model = Recepient
         fields = ['email']
 
+    def __init__(self, *args, **kwargs):
+        super(RequestRecepientForm, self).__init__(*args, **kwargs)
+        self.fields['email'].label = "Recepient's Email Address"
+
 class RequestPaymentRequestForm(forms.ModelForm):
     class Meta:
         model = Request
         fields = ['quantity']
+
+    def __init__(self, *args, **kwargs):
+        super(RequestPaymentRequestForm, self).__init__(*args, **kwargs)
+        self.fields['quantity'].label = "Payment Amount (in USD)"
+
+
 
 # Forms for payment
 
